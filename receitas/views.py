3f442f49d265e60f8db9bd049411269816e56794
Wallas Oliveira Almeida <wallasalmeida607@gmail.com>
2023-04-18 23:33:20 -0300

    att-18-04-2023

--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'receitas/home.html')
-
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-
-def contato(request):
-    return HttpResponse("CONTATO")
+    return render(request, 'receitas/home.html', context={
+        'name': 'Wallas',
+    })
