74471b6063054b3ee8357cced3efa0307f8a0a3b
Wallas Oliveira Almeida <wallasalmeida607@gmail.com>
2023-04-27 02:38:17 -0300

    att-27-04-2023

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'receitas'
 
 urlpatterns = [
-    path('', views.home),
-    path('receitas/<int:id>/', views.receita),
-
+    path('', views.home, name="home"),
+    path('receita/<int:id>/', views.receita, name="receita"),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from utils.receitas.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'receitas/pages/home.html')
+    return render(request, 'receitas/pages/home.html', context={
+        'receitas': [make_recipe() for _ in range(10)],
+    })
 
 
 def receita(request, id):
-    return render(request, 'receitas/pages/receita-view.html')
+    return render(request, 'receitas/pages/receita-view.html', context={
+        'receita': make_recipe(),
+        'is_detail_page': True,
+    })
