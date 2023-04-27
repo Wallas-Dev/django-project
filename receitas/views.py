11b1a35a8bf47f239964bf3b9c6d1a24bd70c181
Wallas Oliveira Almeida <wallasalmeida607@gmail.com>
2023-04-26 21:23:33 -0300

    att-26-04-2023

--- receitas/views.py
+++ receitas/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'receitas/pages/home.html')
+
+
+def receita(request, id):
+    return render(request, 'receitas/pages/receita-view.html')
